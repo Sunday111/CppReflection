6d4f6164929a6b69514873dd9541e18dfb47b69b
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2020-01-12 00:57:07 +0200

    Update common

--- code/include/CppReflection/Common.h
+++ code/include/CppReflection/Common.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "EverydayTools/Preprocessor/ExpotImport.h"
+
 #ifdef INSIDE_CPP_REFLECION_LIB
-    #define CPP_REFLECTION_API __declspec(dllexport)
+    #define CPP_REFLECTION_API EXPORT
 #else
-    #define CPP_REFLECTION_API __declspec(dllimport)
+    #define CPP_REFLECTION_API IMPORT
 #endif
