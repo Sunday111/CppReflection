efbe2cffa3336112368f80525dcd290b4ab595dc
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2023-01-17 11:22:53 +0200

    Try to fight clang warnings

--- code/include/CppReflection/Detail/MakeTypeComparisonOperators.hpp
+++ code/include/CppReflection/Detail/MakeTypeComparisonOperators.hpp
@@ -1,25 +1,33 @@
 #pragma once
 
+#include <bit>
+#include <cstdint>
+
 #include "../TypeComparisonOperators.hpp"
 #include "HasOperator.hpp"
+
+#if !defined(_MSC_VER) || defined(__clang__)
+#define CPP_REFLECT_IGNORE_FLT_WARNING_PUSH \
+  _Pragma("GCC diagnostic push");           \
+  _Pragma("GCC diagnostic ignored \"-Wfloat-equal\"")
+
+#define CPP_REFLECT_IGNORE_FLT_WARNING_POP _Pragma("GCC diagnostic pop")
+#else
+#define CPP_REFLECT_IGNORE_FLT_WARNING_PUSH
+#define CPP_REFLECT_IGNORE_FLT_WARNING_POP
+#endif
 
 namespace cppreflection::detail {
 template <typename T>
 [[nodiscard]] constexpr TypeComparisonOperators MakeTypeComparisonOperators() {
+  CPP_REFLECT_IGNORE_FLT_WARNING_PUSH
   TypeComparisonOperators Result{};
 
   if constexpr (has_equals_operator_v<T>) {
     Result.equals = [](const void* a, const void* b) {
       const T* ca = reinterpret_cast<const T*>(a);
       const T* cb = reinterpret_cast<const T*>(b);
-      #if !defined(_MSC_VER) || defined(__clang__)
-      _Pragma("GCC diagnostic push");
-      _Pragma("GCC diagnostic ignored \"-Wfloat-equal\"");
       return *ca == *cb;
-      _Pragma("GCC diagnostic pop");
-      #else
-      return *ca == *cb;
-      #endif
     };
   }
 
@@ -32,5 +40,6 @@ template <typename T>
   }
 
   return Result;
+  CPP_REFLECT_IGNORE_FLT_WARNING_POP
 }
 }  // namespace cppreflection::detail
