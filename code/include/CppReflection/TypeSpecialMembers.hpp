8198aab2f4c0e80a3235ebba92e30674f5d571d4
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2024-07-28 19:04:26 +0100

    Apply format to some files and minor fixes

--- code/include/CppReflection/TypeSpecialMembers.hpp
+++ code/include/CppReflection/TypeSpecialMembers.hpp
@@ -2,21 +2,24 @@
 
 namespace cppreflection
 {
-    class TypeSpecialMembers
-    {
-    public:
-        using DefaultConstructor = void(*)(void* object);
-        using CopyConstructor = void(*)(void* objectToConstruct, const void* referenceObject);
-        using MoveConstructor = void(*)(void* destinationObject, void* sourceObject);
-        using CopyAssign = void(*)(void* destination, const void* source);
-        using MoveAssign = void(*)(void* destination, void* source);
-        using Destructor = void(*)(void* object);
+class TypeSpecialMembers
+{
+public:
+    [[nodiscard]] constexpr bool operator==(const TypeSpecialMembers&) const = default;
+    [[nodiscard]] constexpr bool operator!=(const TypeSpecialMembers&) const = default;
 
-        DefaultConstructor defaultConstructor = nullptr;
-        CopyConstructor copyConstructor = nullptr;
-        MoveConstructor moveConstructor = nullptr;
-        CopyAssign copyAssign = nullptr;
-        MoveAssign moveAssign = nullptr;
-        Destructor destructor = nullptr;
-    };
-}
+    using DefaultConstructor = void (*)(void* object);
+    using CopyConstructor = void (*)(void* objectToConstruct, const void* referenceObject);
+    using MoveConstructor = void (*)(void* destinationObject, void* sourceObject);
+    using CopyAssign = void (*)(void* destination, const void* source);
+    using MoveAssign = void (*)(void* destination, void* source);
+    using Destructor = void (*)(void* object);
+
+    DefaultConstructor defaultConstructor = nullptr;
+    CopyConstructor copyConstructor = nullptr;
+    MoveConstructor moveConstructor = nullptr;
+    CopyAssign copyAssign = nullptr;
+    MoveAssign moveAssign = nullptr;
+    Destructor destructor = nullptr;
+};
+}  // namespace cppreflection
